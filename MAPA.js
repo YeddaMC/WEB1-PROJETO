b65412f6ccf694ef8379d235f1217b0b94768610
YeddaMC <160504359+YeddaMC@users.noreply.github.com>
2024-12-28 22:11:33 +0000

    Confirmando todas as alterações pendentes

---
+++ MAPA.js
@@ -0,0 +1,9 @@
+//rolagem ao clicar nos links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
